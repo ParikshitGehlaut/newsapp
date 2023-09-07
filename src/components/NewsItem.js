cebd93faccc1c84b9def912b34baaf131dde8991
ParikshitGehlaut <139605784+ParikshitGehlaut@users.noreply.github.com>
2023-09-07 21:34:20 +0530

    committing all changes

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
 
 export class NewsItem extends Component {
+  // constructor() {
+  //   super();
+  //   console.log("I am building newsmonkey");
+  // }
   render() {
-    let { title, description } = this.props; // Deconstructing
+    let { title, description, imageUrl } = this.props; // Deconstructing
     return (
       <div>
         <div className="card" style={{ width: "18rem" }}>
-          <img src="..." className="card-img-top" alt="..." />
+          <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsdetail" className="btn btn-primary">
-              Go somewhere
+            <a href="/newsdetail" className="btn btn-sm btn-primary">
+              Read More
             </a>
           </div>
         </div>
