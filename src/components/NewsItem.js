50c001ca6beb136225ee889f8b1cd5c40ec2f662
ParikshitGehlaut <139605784+ParikshitGehlaut@users.noreply.github.com>
2023-09-10 18:54:53 +0530

    committing all changes

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,15 +6,28 @@ export class NewsItem extends Component {
   //   console.log("I am building newsmonkey");
   // }
   render() {
-    let { title, description, imageUrl } = this.props; // Deconstructing
+    let { title, description, imageUrl, newsUrl } = this.props; // Deconstructing
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img
+            src={
+              !imageUrl
+                ? "https://kchanews.com/wp-content/uploads/2014/09/bigstock-Breaking-News-Screen-36237841.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/newsdetail" className="btn btn-sm btn-primary">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-sm btn-dark"
+            >
               Read More
             </a>
           </div>
